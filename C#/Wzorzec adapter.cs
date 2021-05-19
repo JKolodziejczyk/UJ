2549e7fc220e5e3ed1b6fff2208ebf510fb36409
Jakub Kołodziejczyk <82475576+JKolodziejczyk@users.noreply.github.com>
2021-05-19 13:11:43 +0200

    Add files via upload

---
+++ C#/Wzorzec adapter.cs
@@ -0,0 +1,151 @@
+﻿//Mateusz Basiak Arkadiusz Pospieszny Jakub Kołodziejczyk
+using System;
+using System.IO;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Linq;
+namespace Adapter
+{
+    public interface IXmlFile
+    {
+        public Company[] sendData();
+
+    }
+
+    public interface ILibrary
+    {
+        public void getData(string s);
+    }
+
+    public class Company
+    {
+        public string Name { get; set; }
+        public double Last { get; set; }
+        public double Change { get; set; }
+        public double ChangePerc { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+    }
+
+    public class XmlFile : IXmlFile {
+        private readonly Company[] companies;
+
+        public XmlFile()
+        {
+            string[] Lines = File.ReadAllLines(@"C:\Users\Matbas\Desktop\studia\II rok\IO\Adapter\data.xml");
+            List<Company> tmpCompanies = new List<Company>();
+            for( int i = 1; i<Lines.Length-1; i++)
+            {
+                string[] tmp = Lines[i].Split('"');
+                if (tmp[1] != "N/A" && tmp[3] != "N/A" && tmp[5] != "N/A" && tmp[7] != "N/A" && tmp[9] != "N/A" && tmp[11] != "N/A")
+                {
+                    Company newC = new Company();
+                    newC.Name = tmp[1];
+                    newC.Last = Convert.ToDouble(tmp[3], CultureInfo.InvariantCulture.NumberFormat);
+                    newC.Change = Convert.ToDouble(tmp[5], CultureInfo.InvariantCulture.NumberFormat);
+                    newC.ChangePerc = Convert.ToDouble(tmp[7], CultureInfo.InvariantCulture.NumberFormat);
+                    newC.High = Convert.ToDouble(tmp[9], CultureInfo.InvariantCulture.NumberFormat);
+                    newC.Low = Convert.ToDouble(tmp[11], CultureInfo.InvariantCulture.NumberFormat);
+                    tmpCompanies.Add(newC);
+                }
+            }
+            companies = tmpCompanies.ToArray();
+        }
+
+        public void AverageVals()
+        {
+            double sumChange = 0;
+            double sumHigh = 0;
+            double sumLow = 0;
+            foreach(Company company in companies)
+            {
+                sumChange += company.ChangePerc;
+                sumHigh += company.High;
+                sumLow += company.Low;
+            }
+
+            Console.WriteLine("Srednia zmiana w procentach: " + (sumChange / companies.Length) * 100);
+            Console.WriteLine("Srednia oferta High: " + sumHigh / companies.Length);
+            Console.WriteLine("Srednia oferta Low: " + sumLow / companies.Length);
+        }
+
+        public Company[] sendData()
+        {
+            return companies;
+        }
+    }
+
+    public class Adapter: IXmlFile
+    {
+        private readonly Company[] companies;
+        private readonly ILibrary library;
+
+        public Adapter(ILibrary lib, XmlFile file)
+        {
+            companies = file.sendData();
+            library = lib;
+        }
+
+        public Company[] sendData()
+        {
+            library.getData(JsonSerializer.Serialize(companies));
+            return companies;
+        }
+    }
+
+    public class Library: ILibrary
+    {
+        private string jsonData;
+        
+        public void VariancePositive()
+        {
+            Company[] tmp = JsonSerializer.Deserialize<Company[]>(jsonData);
+            
+            double aveChange = tmp.Select(x => x.Change).Average();
+            double aveChangePerc = tmp.Select(x => x.ChangePerc).Average();
+            double sumChange = 0;
+            double sumChangePerc = 0;
+            
+            foreach(Company company in tmp)
+            {
+                sumChange += Math.Pow((company.Change - aveChange), 2);
+                sumChangePerc += Math.Pow((company.ChangePerc - aveChangePerc), 2);
+            }
+
+            Console.WriteLine("Wariancja dla change: " + sumChange / tmp.Length);
+            Console.WriteLine("Wariancja dla change%: " + sumChangePerc / tmp.Length);
+        }
+
+        public void getData(string data)
+        {
+            Company[] tmp = JsonSerializer.Deserialize<Company[]>(data);
+            List<Company> positive = new List<Company>();
+            foreach(Company company in tmp)
+            {
+                if(company.Change >= 0 && company.ChangePerc >= 0)
+                {
+                    positive.Add(company);
+                }
+            }
+            jsonData = JsonSerializer.Serialize(positive.ToArray());
+            Console.WriteLine("Dane w bibliotece: " + jsonData);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new XmlFile();
+            file.AverageVals();
+
+            var lib = new Library();
+
+            var adapter = new Adapter(lib, file);
+            adapter.sendData();
+            lib.VariancePositive();
+
+        }
+    }
+}
---
+++ C#/Wzorzec fabryka.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Numerics;
+using System.Text.Json;
+namespace Fabryka
+{
+    public interface Furniture
+    {
+        public Vector3 getDim();  //szerokosc dlugosc wysokosc
+        public string getName();
+    }
+
+    public static class Serializer
+    {
+        class Info
+        {
+            public string Name { get; set; }
+            public float Width { get; set; }
+            public float Length { get; set; }
+            public float Height { get; set; }
+
+            public Info(string n, Vector3 dim)
+            {
+                Name = n;
+                Width = dim.X;
+                Length = dim.Y;
+                Height = dim.Z;
+            }
+        }
+        public static string Serialize(Furniture f)
+        {
+            return JsonSerializer.Serialize(new Info(f.getName(), f.getDim()));
+        }
+    }
+
+    public interface Chair
+    {
+        public void sitOn();
+        public bool hasLegs();
+    }
+
+    class VictorianChair : Chair, Furniture
+    {
+        public void sitOn()
+        {
+            Console.WriteLine("Somebody sat down on Victorian Chair!");
+        }
+
+        public bool hasLegs()
+        {
+            return true;
+        }
+
+        public Vector3 getDim()
+        {
+            return new Vector3(6, 4, 5);
+        }
+
+        public string getName()
+        {
+            return $"Victorian Chair";
+        }
+    }
+
+    class ModernChair : Chair, Furniture
+    {
+        public void sitOn()
+        {
+            Console.WriteLine("Somebody sat down on Modern Chair!");
+        }
+
+        public bool hasLegs()
+        {
+            return false;
+        }
+
+        public Vector3 getDim()
+        {
+            return new Vector3(4, 5, 5);
+        }
+
+        public string getName()
+        {
+            return $"Modern Chair";
+        }
+    }
+
+    class ArtDecoChair : Chair, Furniture
+    {
+        public void sitOn()
+        {
+            Console.WriteLine("Somebody sat down on Art Deco Chair!");
+        }
+
+        public bool hasLegs()
+        {
+            return true;
+        }
+
+        public Vector3 getDim()
+        {
+            return new Vector3(7, 4, 4);
+        }
+
+        public string getName()
+        {
+            return $"Art Deco Chair";
+        }
+    }
+
+    public interface Sofa
+    {
+        public void sitOn();
+        public bool hasLegs();
+    }
+
+    class VictorianSofa : Sofa, Furniture
+    {
+        public void sitOn()
+        {
+            Console.WriteLine("Somebody sat down on Victorian Sofa!");
+        }
+
+        public bool hasLegs()
+        {
+            return true;
+        }
+
+        public Vector3 getDim()
+        {
+            return new Vector3(16, 4, 5);
+        }
+
+        public string getName()
+        {
+            return $"Victorian Sofa";
+        }
+    }
+
+    class ModernSofa : Sofa, Furniture
+    {
+        public void sitOn()
+        {
+            Console.WriteLine("Somebody sat down on Modern Sofa!");
+        }
+
+        public bool hasLegs()
+        {
+            return false;
+        }
+
+        public Vector3 getDim()
+        {
+            return new Vector3(14, 5, 5);
+        }
+
+        public string getName()
+        {
+            return $"Modern Sofa";
+        }
+    }
+
+    class ArtDecoSofa : Sofa, Furniture
+    {
+        public void sitOn()
+        {
+            Console.WriteLine("Somebody sat down on Art Deco Sofa!");
+        }
+
+        public bool hasLegs()
+        {
+            return true;
+        }
+
+        public Vector3 getDim()
+        {
+            return new Vector3(17, 4, 4);
+        }
+
+        public string getName()
+        {
+            return $"Art Deco Sofa";
+        }
+    }
+
+    public interface CoffeeTable
+    {
+        public void putThing();
+        public bool hasLegs();
+    }
+
+    class VictorianCoffeTable : CoffeeTable, Furniture
+    {
+        public void putThing()
+        {
+            Console.WriteLine("Somebody put something on Victorian Coffee Table!");
+        }
+
+        public bool hasLegs()
+        {
+            return true;
+        }
+
+        public Vector3 getDim()
+        {
+            return new Vector3(10, 4, 3);
+        }
+
+        public string getName()
+        {
+            return $"Victorian Coffee Table";
+        }
+    }
+    class ModernCoffeeTable : CoffeeTable, Furniture
+    {
+        public void putThing()
+        {
+            Console.WriteLine("Somebody put something on Modern Coffee Table!");
+        }
+
+        public bool hasLegs()
+        {
+            return true;
+        }
+        public Vector3 getDim()
+        {
+            return new Vector3(6, 3, 3);
+        }
+
+        public string getName()
+        {
+            return $"Modern Coffee Table";
+        }
+    }
+    class ArtDecoCoffeeTable : CoffeeTable, Furniture
+    {
+        public void putThing()
+        {
+            Console.WriteLine("Somebody put something on Art Deco Coffee Table!");
+        }
+
+        public bool hasLegs()
+        {
+            return false;
+        }
+        public Vector3 getDim()
+        {
+            return new Vector3(7, 3, 5);
+        }
+
+        public string getName()
+        {
+            return $"Art Deco Coffee Table";
+        }
+    }
+
+    interface FurnitureFactory
+    {
+        public Chair createChair();
+        public Sofa createSofa();
+        public CoffeeTable createCoffeeTable();
+    }
+
+    public class VictorianFurnitureFactory : FurnitureFactory
+    {
+        public Chair createChair()
+        {
+            return new VictorianChair();
+        }
+
+        public Sofa createSofa()
+        {
+            return new VictorianSofa();
+        }
+
+        public CoffeeTable createCoffeeTable()
+        {
+            return new VictorianCoffeTable();
+        }
+    }
+
+    public class ModernFurnitureFactory : FurnitureFactory
+    {
+        public Chair createChair()
+        {
+            return new ModernChair();
+        }
+
+        public Sofa createSofa()
+        {
+            return new ModernSofa();
+        }
+
+        public CoffeeTable createCoffeeTable()
+        {
+            return new ModernCoffeeTable();
+        }
+
+    }
+    public class ArtDecoFurnitureFactory : FurnitureFactory
+    {
+        public Chair createChair()
+        {
+            return new ArtDecoChair();
+        }
+
+        public Sofa createSofa()
+        {
+            return new ArtDecoSofa();
+        }
+
+        public CoffeeTable createCoffeeTable()
+        {
+            return new ArtDecoCoffeeTable();
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ArtDecoFactory = new ArtDecoFurnitureFactory();
+            var VictorianFactory = new VictorianFurnitureFactory();
+            var ModernFactory = new ModernFurnitureFactory();
+
+            var chair1 = ArtDecoFactory.createChair();
+            var sofa1 = ArtDecoFactory.createSofa();
+            var coffee1 = ArtDecoFactory.createCoffeeTable();
+
+            Console.WriteLine(Serializer.Serialize((Furniture)chair1) + "\n" + Serializer.Serialize((Furniture)sofa1) + "\n" + Serializer.Serialize((Furniture)coffee1)+"\n");
+
+            var chair2 = VictorianFactory.createChair();
+            var sofa2 = VictorianFactory.createSofa();
+            var coffee2 = VictorianFactory.createCoffeeTable();
+
+            Console.WriteLine(Serializer.Serialize((Furniture)chair2) + "\n" + Serializer.Serialize((Furniture)sofa2) + "\n" + Serializer.Serialize((Furniture)coffee2) + "\n");
+
+            var chair3 = ModernFactory.createChair();
+            var sofa3 = ModernFactory.createSofa();
+            var coffee3 = ModernFactory.createCoffeeTable();
+
+            Console.WriteLine(Serializer.Serialize((Furniture)chair3) + "\n" + Serializer.Serialize((Furniture)sofa3) + "\n" + Serializer.Serialize((Furniture)coffee3) + "\n");
+        }
+    }
+}
